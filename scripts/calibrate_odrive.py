bc46698accc57ae2c98240e8567f835c8ebe2dbe
Ryan Liao <71594512+Ryan4253@users.noreply.github.com>
2026-05-18 23:18:27 -0400

    Clear odrive errors and check estop in odrive_driver. use odrive.utils helpers (#19)

--- scripts/calibrate_odrive.py
+++ scripts/calibrate_odrive.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-import time
-
 import odrive
+import odrive.utils
 from odrive.enums import AxisState
 
 
@@ -9,19 +8,8 @@ def calibrate_odrive(serial_number: str) -> None:
     print(f"Finding ODrive with serial number {serial_number}...")
     odrv = odrive.find_any(serial_number=serial_number)
 
-    print("Clearing pre-existing errors...")
-    odrv.clear_errors()
-
-    print("Starting calibration...")
     print("There should be a beep and the indicator lights should flash green. Do not touch the robot!")
-    odrv.axis0.requested_state = AxisState.FULL_CALIBRATION_SEQUENCE
-
-    # Wait for the ODrive to leave idle, confirming calibration has started
-    while odrv.axis0.current_state == AxisState.IDLE:
-        time.sleep(0.1)
-
-    while odrv.axis0.current_state != AxisState.IDLE:
-        time.sleep(0.1)
+    odrive.utils.run_state(odrv.axis0, AxisState.FULL_CALIBRATION_SEQUENCE)  # type: ignore[reportUnusedCoroutine]
 
     print(f"ODrive {serial_number} calibrated!")
 
